4c4074547a9b30352febb60791382f0eafb09236
Danial Nurmakhanuly <danial@Danials-MacBook-Air.local>
2026-06-29 12:00:16 +0500

    PWA қосылды

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'mazmunda-v1';
+const urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
